ac4a0837e413c936a2bc81c11bdd9ab90e637550
lioaohi <161966283+lioaohi@users.noreply.github.com>
2024-03-03 09:07:07 +0800

    Update style.js

--- themes/my_theme/style.js
+++ themes/my_theme/style.js
@@ -8,7 +8,7 @@ const Style = () => {
   return <style jsx global>{`
     // 底色
     body{
-        background-color: #eeedee;
+        background-color: #ac6855;
     }
     .dark body{
         background-color: black;
